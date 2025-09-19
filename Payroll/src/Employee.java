ddfb9106441c87a86f6231b40f07e3314937c5ce
Suhgot <shahsaugat@gmail.com>
2025-09-19 15:21:26 -0400

    Employee Class Finished

---
+++ Payroll/src/Employee.java
@@ -0,0 +1,29 @@
+public abstract class Employee {
+	private static int nextEmployeeNumber = 1;
+	private int employeeNumber;
+	
+	private String employeeFirstName;
+	private String employeeLastName;
+	
+	public int getEmployeeNumber(){
+		return employeeNumber;
+	}
+	
+	public String getEmployeeName() {
+		return employeeFirstName + " " + employeeLastName;
+	}
+	
+	public void changeEmployeeName(String First, String Last) {
+		this.employeeFirstName = First;
+		this.employeeLastName = Last;
+	}
+	
+	public Employee (String First, String Last) {
+		this.employeeFirstName = First;
+		this.employeeLastName = Last;
+		this.employeeNumber = nextEmployeeNumber;
+		nextEmployeeNumber++;
+		
+	}
+	public abstract int getPaycheck(); 
+}
